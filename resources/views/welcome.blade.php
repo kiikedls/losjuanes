0a27bbbfc1027db174784b18c447e485b0b968cd
kiikedls <jespinozadelossatnos@gmail.com>
2019-09-27 18:28:25 -0500

    mi nombre

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -90,7 +90,7 @@
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Juan Daniel Cano</a>
-                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laracasts.com">Juan Enrique Espinoza de los santos</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
